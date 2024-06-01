e677318d93589d5a8432a3c99e24ce1fc5143cc1
Vinayak <vinayak192002@gmail.com>
2024-06-01 16:09:00 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//import Accordian from "./components/Accordian";
+import Modal from "./components/CustomModal";
+import UndoCounter from "./components/UndoCounter";
+
+const App = () => {
+  return (
+    <>
+    <UndoCounter/>
+    </>
+  )
+}
+
+export default App
