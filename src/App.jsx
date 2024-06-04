617053e019f0006bd44d8e66e042450452ab90fd
Vinayak <vinayak192002@gmail.com>
2024-06-04 14:50:02 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 //import Accordian from "./components/Accordian";
 // import Modal from "./components/CustomModal";
 // import UndoCounter from "./components/UndoCounter";
-// import ShoppingList from "./components/ShoppingList"
-import MultiStepForms from "./components/MultiStepForms"
+import ShoppingList from "./components/ShoppingList"
+// import MultiStepForms from "./components/MultiStepForms"
 
 const App = () => {
   return (
     <>
-    <MultiStepForms/>
+    <ShoppingList/>
     </>
   )
 }
